f3237f8846be93d714171a49db0b7dea747b9128
Ndibechinonso <edwardndibe@gmail.com>
2021-02-09 22:35:28 +0100

    edits

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const grid = document.querySelector(".photo-grid")
+const loading = document.querySelector(".loading")
 const slide1 = document.querySelector(".slide1")
 const slide2 = document.querySelector(".slide2")
 const slide3 = document.querySelector(".slide3")
@@ -11,9 +12,23 @@ const form = document.getElementById("form")
 const input = document.querySelector(".input")
 
 const getData = query => {
+    loading.style.display = "block";
+    grid.style.display = "none"
     fetch(`https://api.unsplash.com/search/photos?client_id=LTNB5WMcNPir06RnJxoWG8GIAVN-TQhb6tfwzRNybjk&query=${query}&per_page=8`)
         .then(res => res.json())
         .then(data => {
+            // for searches with no result
+            if(data.results.length=== 0) {
+                let emptySearch = document.createElement("h1")
+                emptySearch.innerHTML = `Search Results for "${query}"`
+                let emptyExp = document.createElement("h3")
+                emptyExp.innerHTML = `Sorry, no items were found matching your search keyword`
+                emptySearch.style.width = "800px"
+                emptySearch.style.marginBottom = "100px"
+                emptyExp.style.margin = "auto"
+                slide1.append(emptySearch);
+                slide1.append(emptyExp)
+            }
             data.results.forEach((item, index) => {
                 let imgCard = document.createElement("div")
                 let overlay = document.createElement("div")
@@ -21,12 +36,7 @@ const getData = query => {
                 console.log(data.results)
                 let imgName = document.createElement("h2")
                 let imgLocation = document.createElement("p")
-                if (item.user.first_name === null || item.user.last_name === null || item.user.location === null) {
-                    `${item.user.first_name} === ""`;
-                    `${item.user.last_name} === ""`;
-                    `${item.user.location} === ""`;
-                }
-
+       
                 imgCard.innerHTML = `<img src=${item.urls.thumb} />`
                 imgName.innerHTML = `${item.user.first_name} ${item.user.last_name}`
                 if (item.user.first_name === null) {
@@ -43,7 +53,7 @@ const getData = query => {
                 overlay.append(imgName)
                 overlay.append(imgLocation)
                 imgCard.append(overlay)
-
+                // distributing imgs evenly among the three columns
                 if (index % 3 === 1) {
                     slide1.append(imgCard)
                 }
@@ -65,16 +75,20 @@ const getData = query => {
                     modal.style.display = "none";
                 }
             })
+        }).catch((error)=>{
+            console.log(error)
+        }).finally(()=>{
+            loading.style.display = "none";
+            grid.style.display = "grid";
         })
 }
-
+// enable user to search for a category
 form.addEventListener("submit", e => {
     e.preventDefault();
     slide1.innerHTML = "";
     slide2.innerHTML = "";
     slide3.innerHTML = "";
     getData(input.value);
-    input.value = "";
 });
 
 window.onload = () => getData("african")
